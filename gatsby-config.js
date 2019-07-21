e4a45f1b8646cad531e2371d6e8225b525b7fc72
nickFalcone <nfalcone@u.rochester.edu>
2019-07-21 10:46:26 -0400

    move GA to body, remove sample rate

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,13 +45,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-143613685-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["//**", "/do-not-track/me/too/"],
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "nickandalesa.com",
+        head: false,
       },
     },
   ],
