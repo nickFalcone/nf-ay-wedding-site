76f228687ffe0f58c8057c4c4c3694014f0f9f85
nickFalcone <nfalcone@u.rochester.edu>
2019-10-12 17:04:10 -0400

    Get sitemap URLs and pipe into sites.txt file for batch lighthouse reporting

--- report/lighthouse.sh
+++ report/lighthouse.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 . ~/.nvm/nvm.sh
-nvm use 10.16.3 && lighthouse-batch -f report/sites.txt
+nvm use 10.16.3
+curl https://www.nickandalesa.com/sitemap.xml | \
+grep -e 'loc' | sed -e 's/.*<loc>\(.*\)<\/loc>.*/\1/' > report/sites.txt
+lighthouse-batch -f report/sites.txt
